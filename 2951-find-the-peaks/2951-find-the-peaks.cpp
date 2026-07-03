aa28c69401717abcf8bdd732269f90978d6baf52
Dharmendra <dharmendrasolanki2004@gmail.com>
2026-07-03 16:07:29 +0530

    Time: 0 ms (100%), Space: 24.5 MB (73.56%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.cpp
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> findPeaks(vector<int>& mountain) {
         int n = mountain.size();
         vector<int> ans;
-        if(mountain.size() == 1) ans.push_back(0);
 
         for(int i=1; i<n-1; i++){
             if(mountain[i-1] < mountain[i] && mountain[i] > mountain[i+1]){
