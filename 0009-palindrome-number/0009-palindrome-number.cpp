eaee0e1f4a753dde4c7fe3f593fda2a77b094595
Dharmendra <dharmendrasolanki2004@gmail.com>
2026-07-29 22:43:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.62%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        long long reverse = 0;
+        long long xcopy = x;
+        while(x > 0){
+            reverse = (reverse * 10) + (x%10);
+            x = x/10;
+        }
+        return reverse == xcopy;
+    }
+};
